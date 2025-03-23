3f92bdd16d8e4cab52917f30c5d4573a236cb6c8
LoiTo <tvloi3999@gmail.com>
2025-03-23 13:34:37 +0700

    COMMON | add log for crash global

--- windows/sandbox/App.xaml.cs
+++ windows/sandbox/App.xaml.cs
@@ -1,8 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
 using Microsoft.ReactNative;
 using sandbox.Common;
+using sandbox.Service;
+using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
+using Windows.Data.Json;
+using Windows.Storage;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Shapes;
+using Path = System.IO.Path;
 
 namespace sandbox
 {
@@ -29,8 +41,45 @@ namespace sandbox
             Microsoft.ReactNative.Managed.AutolinkedNativeModules.RegisterAutolinkedNativeModulePackages(PackageProviders); // Includes any autolinked modules
 
             PackageProviders.Add(new ReactPackageProvider());
-     
+            LogService.Init();
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            Windows.UI.Xaml.Application.Current.UnhandledException += Current_UnhandledException;
             InitializeComponent();
+        }
+
+        private void crashLog(string lines)
+        {
+            string[] array = new string[10];
+            Array.Fill(array, "=");
+            string[] contentParticals = new string[]
+            {
+                string.Join(" ", array),
+                DateTime.Now.ToString(),
+                string.Join(" ", array)
+            };
+            string headerTime = string.Join(" ", contentParticals);
+            File.AppendAllLines(LogService.filePath, new string[]{ headerTime, lines });
+        }
+
+
+        // Handles all unhandled exceptions from the application
+        private void CurrentDomain_UnhandledException(object sender, System.UnhandledExceptionEventArgs e)
+        {
+            if (e.ExceptionObject is Exception ex)
+            {
+                // Log the exception or show a message (depending on your preference)
+                crashLog(ex.ToString());
+            }
+        }
+
+        // Handles unhandled exceptions specific to the UWP/WinUI framework
+        private void Current_UnhandledException(object sender, Windows.UI.Xaml.UnhandledExceptionEventArgs e)
+        {
+            if (e.Exception is Exception ex)
+            {
+                crashLog(ex.ToString());
+            }
+            e.Handled = true;
         }
 
         /// <summary>
---
+++ windows/sandbox/Service/Log.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace sandbox.Service
+{
+    class LogService
+    {
+        private static string currentPath = ApplicationData.Current.LocalFolder.Path;
+        private static string keyFileName = "log.dat";
+        private Dictionary<string, string> keyValueStore;
+        public static string filePath   // property
+        {
+            get { return Path.Combine(dirPath, keyFileName); }   // get method
+        }
+
+        public static string dirPath   // property
+        {
+            get { return Path.Combine(currentPath, "log"); }   // get method
+        }
+
+
+        public static void Init()
+        {
+            if (!Directory.Exists(dirPath))
+            {
+                Directory.CreateDirectory(dirPath); // Create the directory if it doesn't exist
+                Console.WriteLine("Directory created successfully.");
+                using (FileStream fs = File.Create(filePath))
+                {
+                    Console.WriteLine("File created successfully.");
+                }
+            }
+        }
+
+    }
+}
